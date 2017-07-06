e25867b672cc9188a5301cb63c3a6bc7d3b188c1
gaargly <lirasaman@gmail.com>
2017-07-05 17:38:05 -0700

    added timer, refactored styles

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 
     // Get a reference to the link on the page
     // with an id of "mylink"
-    var a = document.getElementById('button');
+    var randomize = document.getElementById('randomize');
 
     // Set code to run when the link is clicked
     // by assigning a function to "onclick"
-    a.onclick = function() {
+    randomize.onclick = function() {
 
       // Gets user input
       var text = document.getElementById('input').value;
@@ -25,4 +25,38 @@
       return false;
 
     }
+
+    /* Timer */
+    var start = document.getElementById('start');
+    start.classList.add("hide");
+
+    start.onclick = function() {
+      this.classList.add("hide");
+      var timeoutHandle;
+      function countdown(minutes) {
+          var seconds = 60;
+          var mins = minutes
+          function tick() {
+              var counter = document.getElementById("timer");
+              var current_minutes = mins-1
+              seconds--;
+              counter.innerHTML =
+              current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+              if( seconds > 0 ) {
+                  timeoutHandle=setTimeout(tick, 1000);
+              } else {
+
+                  if(mins > 1){
+
+                     // countdown(mins-1);   never reach “00″ issue solved:Contributed by Victor Streithorst
+                     setTimeout(function () { countdown(mins - 1); }, 1000);
+
+                  }
+              }
+          }
+          tick();
+      }
+      countdown(25);
+    }
+
   }
